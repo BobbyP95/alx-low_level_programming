7d334cba967c23189638753fa6009032829ece0e
BobbyP95 <bobby.bp95@gmail.com>
2022-04-25 20:04:51 +0000

    commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,11 +14,11 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
-typedef struct list_t
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_t *next;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
